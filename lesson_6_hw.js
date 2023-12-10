175c73dc92ecaa5fd3026ba0d27059ac41c81124
Yuliia Dziuba <uliad540@gmail.com>
2023-12-10 14:07:03 +0200

    Hometask 6 JS Arrays

---
+++ lesson_6_hw.js
@@ -0,0 +1,74 @@
+/*
+1. Написати функцію, яка перетворює строку на масив слів,
+    сортує його за алфавітним порядком і повертає (у масиві не повинно бути пробілів як елементів)
+ */
+
+const str = "Це довільний текст для створення масиву слів ";
+ 
+    function stringToArray(str) {
+      str = str.trim().toLowerCase().split(' ').sort()
+        return str
+    }
+
+
+console.log(stringToArray(str));    // виводить [ 'для', 'довільний', 'масиву', 'слів', 'створення', 'текст', 'це' ]
+
+/*
+2. Написати функцію, яка видаляє з масива елементи, що дублюються, та повертає масив оригінальних елементів,
+відсортованих за зростанням
+ */
+
+const initialArray = [1, 4, 6, 6, 7, 5, 34, 5, 1000, 66, 1];
+
+function asc (a, b) {
+    return a-b
+  }
+  function removeDuplicatesAndSort(arr) {
+      let unique = []
+      arr.forEach(element => {
+          if(!unique.includes(element)) {
+              unique.push(element)
+          }
+      }); return unique.sort(asc)
+    }
+      console.log(removeDuplicatesAndSort(initialArray)); // виводить [ 1, 4, 5, 6, 7, 34, 66, 1000 ]
+/*
+3. Написати функцію, яка поверне масив парних чисел
+ */
+const initialArray2 = [2, 3, 6, 7, 9, 12];
+function arrayOfEvens(arr) {
+    let arr2 = []
+    arr.forEach (element => {
+        if(element%2 === 0) {
+            arr2.push(element)
+        }
+    }); return arr2
+}
+
+console.log(arrayOfEvens(initialArray2)); // виводить [ 2, 6, 12 ]
+
+/*
+4. Написати функцію, яка сформує масив з послідовності чисел, формуючи його з аргумента, який збільшує
+кожний наступний елемент послідовності на самого себе. Максимальне значення масиву не повинно перевищувати число 30.
+ */
+function createArray(element) {
+    
+    const mas = [element]
+    //mas.unshift(element)
+    while (true) {
+      const prev = mas[mas.length-1]
+      const newElem = prev + element
+      if (newElem > 30) {
+        return mas
+      }
+      mas.push(newElem)
+    }
+  }
+
+/*function increase(arg) {
+    return arg + arg;
+  }*/
+
+console.log(createArray(5)); // виведе [ 5, 10, 15, 20, 25, 30 ]
+console.log(createArray(3)); // виведе [ 3,  6,  9, 12, 15, 18, 21, 24, 27, 30 ]
+
